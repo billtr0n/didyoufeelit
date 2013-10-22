7e6a617c69f84eaa97d319fc360b89faf1f2875b
William Savran <wsavran@gmail.com>
2013-10-22 14:42:08 -0700

    added functionality to start the actual app - right now everything is static.  update things to be dynamic in   the future.
    next: need to update the horizontal paging.

--- didyoufeelit/src/com/example/didyoufeelit/MainActivity.java
+++ didyoufeelit/src/com/example/didyoufeelit/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.didyoufeelit;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,20 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void continueProgram(View view) {
+    	// always need intent
+    	Intent intent = new Intent(this, com.example.didyoufeelit.OpenHoriztonalView.class);
+    	startActivity(intent);
+    }
+    
+    public void closeProgram(View view) {
+    	// Always need intent
+    	Intent intent = new Intent(Intent.ACTION_MAIN);
+    	intent.addCategory(Intent.CATEGORY_HOME);
+    	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    	startActivity(intent);
     }
     
 }
---
+++ didyoufeelit/src/com/example/didyoufeelit/OpenHoriztonalView.java
@@ -0,0 +1,12 @@
+package com.example.didyoufeelit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpenHoriztonalView extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
