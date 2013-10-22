b607c212ed9e89db766d53ec0df606c574bc8579
William Savran <wsavran@gmail.com>
2013-10-22 15:49:22 -0700

    added horizontal paging view. - temporary place holders. - think about design for this part

--- didyoufeelit/src/com/example/didyoufeelit/OpenHoriztonalView.java
+++ didyoufeelit/src/com/example/didyoufeelit/OpenHoriztonalView.java
@@ -2,11 +2,16 @@ package com.example.didyoufeelit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 
 public class OpenHoriztonalView extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.pager_main);
+		HorizontalPagerAdapter adapter = new HorizontalPagerAdapter();
+		ViewPager myPager = (ViewPager) findViewById(R.id.viewpager);
+		myPager.setAdapter(adapter);
+		myPager.setCurrentItem(0);
 	}
 }
